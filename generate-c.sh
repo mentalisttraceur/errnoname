54c1c079503909e38a02554a44ce4e28fdc026e9
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2020-01-26 03:40:41 +0000

    Separate `sed` steps by conceptual boundaries

--- generate-c.sh
+++ generate-c.sh
@@ -26,38 +26,48 @@ only_after()
     sed "1,/$1/ d"
 }
 
-format_as_array_entries()
+wrap_in_ifdef_blocks()
 {
     # Turns lines like
     #     EAGAIN
     # into lines like
-    #     #ifdef EAGAIN
-    #         [EAGAIN] = "EAGAIN",
-    #     #endif
-
-    sed 's/^.*$/    #ifdef &\n        [&] = "&",\n    #endif/'
-}
-
-format_as_switch_entries()
-{
-    # Turns lines like
+    #         #ifdef EAGAIN
     #     EAGAIN
-    # into lines like
-    #     #ifdef EAGAIN
-    #         case EAGAIN: return "EAGAIN";
-    #     #endif
+    #         #endif
 
-    sed 's/^.*$/    #ifdef &\n        case &: return "&";\n    #endif/'
+    sed 's/^.*$/    #ifdef &\n&\n    #endif/'
 }
 
 skip_1_if_same_as_2()
 {
-    # Given the arguments 'EWOULDBLOCK' and 'EAGAIN', turns lines like
-    #     #ifdef EWOULDBLOCK
+    # Given arguments "EWOULDBLOCK" and "EAGAIN", turns lines like
+    #     EWOULDBLOCK
     # into lines like
-    #     #if defined(EWOULDBLOCK) && (!defined(EAGAIN) || EWOULDBLOCK != EAGAIN)
+    #             #if !defined(EAGAIN) || EAGAIN != EWOULDBLOCK
+    #     EWOULDBLOCK
+    #             #endif
 
-    sed "s/ifdef $1/if defined($1) \&\& (!defined($2) || $1 != $2)/"
+    sed "/#/! s/$1/        #if !defined($2) || $1 != $2\n$1\n        #endif/"
+}
+
+format_as_array_designated_initializers()
+{
+    # Turns lines like
+    #     EAGAIN
+    # into lines like
+    #             [EAGAIN] = "EAGAIN",
+
+    sed '/#/! s/^.*$/        [&] = "&",/'
+}
+
+format_as_switch_cases()
+{
+    # Turns lines like
+    #     EAGAIN
+    # into lines like
+    #             case EAGAIN: return "EAGAIN";
+
+    sed '/#/! s/^.*$/        case &: return "&";/'
 }
 
 # errno list comes in on stdin, errnoname.c comes out on stdout
@@ -69,22 +79,24 @@ cat errnoname.c.template \
 | only_up_to '{{ array_entries }}'
 
 printf '%s\n' "$errno_list" \
-| format_as_array_entries \
+| wrap_in_ifdef_blocks \
 | skip_1_if_same_as_2 EWOULDBLOCK EAGAIN \
 | skip_1_if_same_as_2 EOPNOTSUPP ENOTSUP \
 | skip_1_if_same_as_2 EDEADLOCK EDEADLK \
-| skip_1_if_same_as_2 ECANCELLED ECANCELED
+| skip_1_if_same_as_2 ECANCELLED ECANCELED \
+| format_as_array_designated_initializers
 
 cat errnoname.c.template \
 | only_after '{{ array_entries }}' \
 | only_up_to '{{ switch_entries }}'
 
 printf '%s\n' "$errno_list" \
-| format_as_switch_entries \
+| wrap_in_ifdef_blocks \
 | skip_1_if_same_as_2 EWOULDBLOCK EAGAIN \
 | skip_1_if_same_as_2 EOPNOTSUPP ENOTSUP \
 | skip_1_if_same_as_2 EDEADLOCK EDEADLK \
-| skip_1_if_same_as_2 ECANCELLED ECANCELED
+| skip_1_if_same_as_2 ECANCELLED ECANCELED \
+| format_as_switch_cases
 
 cat errnoname.c.template \
 | only_after '{{ switch_entries }}'
