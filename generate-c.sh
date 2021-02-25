13aab9895b878458cde3a8e63605ed2d1897b343
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2021-02-25 09:54:39 +0000

    generate-c.sh: reorganize to improve top-down readability

--- generate-c.sh
+++ generate-c.sh
@@ -12,6 +12,33 @@
 # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
+main()
+{
+    # errno list comes in on stdin, errnoname.c comes out on stdout
+
+    # spooling it all into memory is easiest way to use it twice
+    errno_list=`cat`
+
+    cat errnoname.c.template \
+    | only_lines_before '{{ array_entries }}'
+
+    printf '%s\n' "$errno_list" \
+    | wrap_in_preprocessor_checks \
+    | format_as_array_designated_initializers
+
+    cat errnoname.c.template \
+    | only_lines_after '{{ array_entries }}' \
+    | only_lines_before '{{ switch_entries }}'
+
+    printf '%s\n' "$errno_list" \
+    | wrap_in_preprocessor_checks \
+    | format_as_switch_cases
+
+    cat errnoname.c.template \
+    | only_lines_after '{{ switch_entries }}'
+}
+
+
 only_lines_before()
 {
     # Only prints the lines before the line
@@ -26,6 +53,18 @@ only_lines_after()
     # that matches the first argument.
 
     sed "1,/$1/ d"
+}
+
+wrap_in_preprocessor_checks()
+{
+    wrap_in_ifdef_blocks \
+    | handle_possible_alias EAGAIN EWOULDBLOCK \
+    | handle_possible_alias ENOTSUP EOPNOTSUPP \
+    | handle_possible_alias EDEADLK EDEADLOCK \
+    | handle_possible_alias ECANCELED ECANCELLED \
+    | handle_possible_alias ECONNREFUSED EREFUSED \
+    | handle_possible_alias EDESTADDRREQ EDESTADDREQ \
+    | handle_possible_alias EINPROGRESS EINPROG
 }
 
 wrap_in_ifdef_blocks()
@@ -52,18 +91,6 @@ handle_possible_alias()
     sed "/#/! s/^$2$/        #if !defined($1) || $2 != $1\n$2\n        #endif/"
 }
 
-wrap_in_preprocessor_checks()
-{
-    wrap_in_ifdef_blocks \
-    | handle_possible_alias EAGAIN EWOULDBLOCK \
-    | handle_possible_alias ENOTSUP EOPNOTSUPP \
-    | handle_possible_alias EDEADLK EDEADLOCK \
-    | handle_possible_alias ECANCELED ECANCELLED \
-    | handle_possible_alias ECONNREFUSED EREFUSED \
-    | handle_possible_alias EDESTADDRREQ EDESTADDREQ \
-    | handle_possible_alias EINPROGRESS EINPROG
-}
-
 format_as_array_designated_initializers()
 {
     # Turns lines like
@@ -84,25 +111,4 @@ format_as_switch_cases()
     sed '/#/! s/^.*$/        case &: return "&";/'
 }
 
-# errno list comes in on stdin, errnoname.c comes out on stdout
-
-# spooling it all into memory is easiest way to use it twice
-errno_list=`cat`
-
-cat errnoname.c.template \
-| only_lines_before '{{ array_entries }}'
-
-printf '%s\n' "$errno_list" \
-| wrap_in_preprocessor_checks \
-| format_as_array_designated_initializers
-
-cat errnoname.c.template \
-| only_lines_after '{{ array_entries }}' \
-| only_lines_before '{{ switch_entries }}'
-
-printf '%s\n' "$errno_list" \
-| wrap_in_preprocessor_checks \
-| format_as_switch_cases
-
-cat errnoname.c.template \
-| only_lines_after '{{ switch_entries }}'
+main "$@"
